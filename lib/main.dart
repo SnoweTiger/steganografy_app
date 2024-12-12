d775c7ec1f004397fca866b6c07a039bfd41299e
SnoweTiger <snowetiger@gmail.com>
2024-12-12 18:59:43 +0300

    add windows to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:steganografy_app/screens/home.dart';
+import 'package:steganografy_app/utils/constants.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(windowsWidth, windowsHeight),
+    minimumSize: Size(windowsWidth, windowsHeight),
+    center: true,
+    backgroundColor: Colors.transparent,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(const MyApp());
 }
 
@@ -12,10 +28,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Steganografy App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
-        useMaterial3: true,
-      ),
       home: const Home(),
     );
   }
